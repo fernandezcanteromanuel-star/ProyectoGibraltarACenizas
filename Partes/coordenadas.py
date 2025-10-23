81c0cf3e90e1f8ecdc4748184274cf21e7b039f9
fernandezcanteromanuel-star <fernandezcanteromanuel@gmail.com>
2025-10-23 17:55:51 +0200

    plano

--- Partes/coordenadas.py
+++ Partes/coordenadas.py
@@ -1,4 +1,18 @@
 # Generamos unas coordenadas x, y
+from math import sqrt
 
-class Plano:
-    pass
+class Punto:
+    #Definimos las coordenadas de nuestro plano con funciones que nos permitan
+    #hacer las operaciones necesarias
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    #Función para medir distancias entre puntos
+    def distancia(self, p2):
+        dist = sqrt((self.x - p2.x)**2+(self.y-p2.y)**2)
+        return dist
+
+
+
+
